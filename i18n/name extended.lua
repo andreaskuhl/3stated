adf90e93da7180a9c26f724d3b7d43ee5bf48462
andreaskuhl <andreas@kuhl-familie.de>
2025-09-07 12:10:37 +0200

    add: create i18n files for name as ASCII and extended version
    This is for simplify tests: only copy the  ASCII or extended version file to name.lua.

---
+++ i18n/name extended.lua
@@ -0,0 +1,9 @@
+return
+{
+  cs = "3-stavový-displej",
+  de = "3-Zustand-Anzeige",
+  en = "3-State-Display",
+  es = "3-Estados-Indicador",
+  fr = "3-États-Affichage",
+  it = "3-Stati-Display",
+}
